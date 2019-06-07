20bbdfac1dec57f600d58f480f97fd92b98fd1fb
Initial Commit <>
2019-06-08 00:15:50 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,47 +1,62 @@
+document.addEventListener("DOMContentLoaded", function(event) {
 
-const cards = [
-{
-rank: "queen", 
-suit: "hearts",
-cardImage: "images/queen-of-hearts.png", 
-},
-{
-rank: "queen", 
-suit: "diamonds",
-cardImage: "images/queen-of-diamonds.png", 
-},
-{
-rank: "king", 
-suit: "hearts",
-cardImage: "images/king-of-hearts.png", 
-},
-{
-rank: "king", 
-suit: "diamonds",
-cardImage: "images/king-of-diamonds.png", 
-}
+var cards = [
+	{
+	rank: "queen", 
+	suit: "hearts",
+	cardImage: "img/queen-of-hearts.png", 
+	},
+	{
+	rank: "queen", 
+	suit: "diamonds",
+	cardImage: "img/queen-of-diamonds.png", 
+	},
+	{
+	rank: "king", 
+	suit: "hearts",
+	cardImage: "img/king-of-hearts.png", 
+	},
+	{
+	rank: "king", 
+	suit: "diamonds",
+	cardImage: "img/king-of-diamonds.png", 
+	}
 ]; 
-const cardsInPlay = [];
 
-function checkForMatch(){
+var cardsInPlay = [];
+
+var checkForMatch = function() {
+	// cardElement.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match");
 	} else {
 		alert("Sorry, try again");	
 	}	
-};
+}
 
-function flipCard (cardId){
-	console.log("User flipped " + cards[cardId].rank);
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 		}
 };
+var createBoard = function(){
+	for(var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','img/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard)   
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+//runs above code to play game
+createBoard();
+});
 
-
-flipCard(0); 
-flipCard(2);
+//Allows you to link button to restart game
+function reloadGame() {
+	window.location.reset();
+};
